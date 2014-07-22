562191fea5881389d8b6dc86c18192de93652d83
Michael Rose <michael@rosenetwork.net>
2014-07-21 18:56:43 -0700

    current state

--- fish/vi3.fish
+++ fish/vi3.fish
@@ -341,14 +341,15 @@ function restoreme
 end
 
 function vi3_firstrun
-   cat ~/.config/fish/config.fish /opt/vi3/fishconfig.txt > /tmp/tmpvi3fish
-   cp /tmp/tmpvi3fish ~/.config/fish/config.fish
-   cp /opt/vi3/personalconfigexample ~/.i3/personalconfig
-   mkdir ~/sessions
-   mkdir ~/.i3/sessions
-   mkdir ~/.i3/colors
-   set -U colors greenandyellow
-   update-vi3-config
+    cat ~/.config/fish/config.fish /opt/vi3/fishconfig.txt > /tmp/tmpvi3fish
+    cp /tmp/tmpvi3fish ~/.config/fish/config.fish
+    cp /opt/vi3/personalconfigexample ~/.i3/personalconfig
+    cp /opt/vi3/template.json ~/.i3/sessions/template.json
+    mkdir ~/sessions
+    mkdir ~/.i3/sessions
+    mkdir ~/.i3/colors
+    set -U colors greenandyellow
+    update-vi3-config
 end
 
 function pi3
