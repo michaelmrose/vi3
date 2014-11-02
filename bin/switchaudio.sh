dc0a8eb3386cba667745b5f5e29d806eafb35117
Michael Rose <michael@rosenetwork.net>
2014-11-02 15:46:12 -0800

    current state

--- bin/switchaudio.sh
+++ bin/switchaudio.sh
@@ -13,10 +13,10 @@ fi
 pacmd "set-default-sink ${next_sink_index}"
 
 #move all inputs to the new sink
-for app in $(pacmd list-sink-inputs | sed -n -e 's/index:[[:space:]]\([[:digit:]]\)/\1/p');
-    do
-        pacmd "move-sink-input $app $next_sink_index"
-done
+# for app in $(pacmd list-sink-inputs | sed -n -e 's/index:[[:space:]]\([[:digit:]]\)/\1/p');
+#     do
+#         pacmd "move-sink-input $app $next_sink_index"
+# done
 
 ff setme default_sink $next_sink_index
 
@@ -25,7 +25,8 @@ declare -i ndx=0
 pacmd list-sinks | sed -n -e 's/device.description[[:space:]]=[[:space:]]"\(.*\)"/\1/p' | while read line;
 do
     if [ $next_sink_index -eq $ndx ] ; then
-        notify-send -i notification-audio-volume-high "Sound output switched to" "$line"
+        #notify-send -i notification-audio-volume-high "Sound output switched to" "$line"
+        ff msg --id 8 -t "Sound output switched to $line"
         exit
     fi
     ndx+=1
