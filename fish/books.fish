f2e236bfa7103c1cb29605bdf2344e88e965190a
Michael Rose <michael@rosenetwork.net>
2015-03-24 00:17:01 -0700

    vastly improved version of books.fish

--- fish/books.fish
+++ fish/books.fish
@@ -1,24 +1,37 @@
 function books
-   
-    # test if we have called without args so we wont try to expand args that don't exist 
-    if not exists $argv
-        show-recent-reads
-        return 0
-    end
-    
-    if substr $argv $HOME/calibre
-        open-book $argv
-        return 0
-    end
+    open-book-by-title (select-book (query-calibre-title (return-query $argv)))
+end
 
-    # selector is going to be the first arg and if it turns out to be a valid criteria
-    # like author we are going to end up assuming the rest is the query
-    # otherwise the implicit criteria is title and the the whole of the remaining args
-    # make up the query  
+function query-calibre
+    set type $argv[1]
+    set field $argv[2]
+    set query $argv[3..-1]
+    switch $type
+        case "exact"
+            set com (echo calibredb list --fields formats,title,$field -s {$field}:\\\"=\"$query\"\\\" --for-machine)
+        case "contains"
+            set com (echo calibredb list --fields formats,title,$field -s $field:$query --for-machine)
+    end
+    eval $com
+end
 
+function query-calibre-title
+    set result (query-calibre contains $argv | jq .[].title)
+    set exact (println $result | grep -i "$argv[2..-1]")
+    if exists $exact
+        println $exact
+    else
+        println $result
+    end
+end
+
+function query-calibre-formats
+    query-calibre exact $argv | jq .[].formats[]
+end
+
+function return-query
     set selector $argv[1]
-    set criteria author title publisher series tag tags
-    set formats pdf epub mobi djvu lit chm txt
+    set criteria author title publisher series tags
     
     if contains $selector $criteria
         set query $argv[2..-1]
@@ -26,99 +39,50 @@ function books
         set selector title
         set query $argv
     end
+    println $selector $query
+end
 
-    set filenames (find-books $selector $query)
-    set books (apply extract-title filenames | sort | uniq)
+function open-book-file
+    set com open (quote $argv)
+    add-to-recent-reads $argv
+    eval $com
+end
+
+
+function open-book-by-title
+    set title (stripquotes $argv)
+    set files (query-calibre-formats title $title)
+    set name (stripquotes (get-fname-of-file $files[1]))
     
-    # kind of non obvious but this sets the value of a variable named for each filetype to the
-    # value of a file of that type useful once we have it narrowed down to one book as
-    # pdf will equal the name of the pdf version epub the epub version and so on
-
-    for i in $filenames
-        set filetype (extract-filetype $i)
-        set $filetype $i
-    end
-
-    set nbooks (count $books)
-
-    switch $nbooks
-        case 0
-            echo "Search produced no results"
-        case 1
-            for i in $formats
-                if contains $$i $filenames
-                    # $$i will contain the value of $pdf if $i is pdf
-                    # and if it exists in filenames we have found what we want to open
-                    set target $$i
-                    break
-                end
-            end
-            
-        case "*"
-            # given many results find out if one is an exact match
-            for i in $books
-                set val (echo $i | trim)
-                if match $val $query
-                    #ok so one is an exact match
-                    for j in $filenames
-                        set title (echo $j | extract-title)
-                        # lets find the filename which contains the title 
-                        # that we want and open it
-                        if match $title $query
-                            set target $$i
-                        end
-                    end
-                    #now find a each of the suitable formats see if there is a match for it in filenames
-                    for i in $formats
-                        if contains $$i $filenames
-                            set target $$i
-                            break
-                        end
-                    end
-                end
-            end
-        end
-      
-        if exists $target
-            add-to-recent-reads (echo $target | extract-title)
-            open $target
-        else
-            set choice (println $books | sed 's/_/:/g' | dm menu "books")
-            if exists $choice
-                books $choice
-            else
-                return 0
-            end
-        end
+    set com open (quote (choose-format $name))
+    add-to-recent-reads $title
+    eval $com
 end
 
-function find-books
-    set selector $argv[1]
-    set query $argv[2..-1]
-    set correctedquery (echo $query | sed 's/:/_/g')
-    set returnval (calibredb list -s "$selector:~^.*"{$query}"" -f formats --for-machine | grep /home | cut -d '"' -f2 | grep -i "$correctedquery")
-    println $returnval
- end
+function choose-format
+    set formats pdf epub mobi djvu lit chm txt
+    set name $argv
 
-function extract-title
-    if exists $argv
-        echo $argv | cut -d "." -f1 | cut -d "/" -f7 | cut -d "(" -f1 | trim | sed 's/\\\//g'
-    else
-        while read -l line
-            echo $line | cut -d "." -f1 | cut -d "/" -f7 | cut -d "(" -f1 | trim | sed 's/\\\/ /g'
+    for i in $formats
+        if test -f $name.$i
+            echo $name.$i
+            return 0
         end
+    end
+    return 1
+end
+        
+
+function select-book
+    if test (count $argv) -gt 1
+        println $argv | dm menu "select book"
+    else
+        echo $argv
     end
 end
 
-function extract-filetype
-    if exists $argv
-        echo (cutlast . $argv)
-    else
-        while read -l line
-            echo (cutlast . $line)
-        end
-        return 0
-    end
+function add-to-recent-reads
+    #totally rewrite this
 end
 
 function show-recent-reads
@@ -128,36 +92,4 @@ function show-recent-reads
     else
         return 0
     end
-end
-
-function totitle
-    echo $argv | sed 's/_/:/g'
-end
-
-function tofname
-    echo $argv | sed 's/:/_/g'
-end
-
-function add-to-recent-reads
-    set -l maxreads 20
-    set -l ndx (math "$maxreads +1")
-    set realtitle (totitle $argv)
-    set -U recent_reads $recent_reads $realtitle
-    set recent_reads (println $recent_reads | sort | uniq)
-    if test (count $recent_reads) -gt $maxreads
-        set recent_reads $recent_reads[2..$ndx]
-    end
-end
-
-function get-fname-of-title
-    echo (calibredb list --fields formats -s "title:=$argv" --for-machine | grep '"/' | cut -d '"' -f2)
-end
-
-function unique
-    for i in $argv
-        if not contains $acc $i
-            set acc $acc $i
-        end
-    end
-    println $acc
 end
