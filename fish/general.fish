aad1a9e070403c7cb41da00076e27e4cf103e790
Michael Rose <michael@rosenetwork.net>
2015-07-11 23:19:31 +0000

    moved pathof

--- fish/general.fish
+++ fish/general.fish
@@ -386,3 +386,15 @@ function without
         end
     end
 end
+
+function pathof
+    set partialpath (pwd)/$argv
+    set fullpath $argv
+    if test -f $partialpath
+        echo $partialpath
+    else if test -f $fullpath
+        echo $fullpath
+    else
+        echo not a file!
+    end
+end
